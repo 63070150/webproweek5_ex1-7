b97e2c30262e3601df5a3beaba601db01bac22f0
63070150 <63070150@it.kmitl.ac.th>
2023-02-17 09:33:18 +0700

    done

---
+++ ex1/ex1.js
@@ -0,0 +1,11 @@
+
+var app = new Vue({
+    el: '#ex1',
+    data: {
+        student_id: '63070150',
+        student_fname: "Rammakorn",
+        student_lname: "Wongjun",
+        color: 'https://www.google.com/',
+        ok: false,
+    }
+})
